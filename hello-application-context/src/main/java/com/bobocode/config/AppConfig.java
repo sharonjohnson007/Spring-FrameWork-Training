91dacf2733bba27949508dde02d5c24153c8128d
Sharon Johnson <sharonjohnson.manuvel@gcitsloutions.com>
2023-01-17 13:08:13 +0530

    pending spring framework exercises

---
+++ hello-application-context/src/main/java/com/bobocode/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.bobocode.config;
+
+import com.bobocode.TestDataGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This class application context configuration.
+ * <p>
+ * todo: make this class a Spring configuration class
+ * todo: enable component scanning for dao and service packages
+ * todo: provide explicit configuration for a bean of type {@link TestDataGenerator} with name "dataGenerator" in this class.
+ * todo: Don't specify bean name "dataGenerator" explicitly
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.bobocode.dao","com.bobocode.service"})
+public class AppConfig {
+    @Bean
+    public TestDataGenerator dataGenerator() {
+        return new TestDataGenerator();
+    }
+
+}
---
+++ transactional-user-service/src/main/java/com/bobocode/config/JpaConfig.java
@@ -0,0 +1,52 @@
+package com.bobocode.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * {@link JpaConfig} provides configuration required to create {@link javax.persistence.EntityManagerFactory}. In order
+ * to configure {@link javax.persistence.EntityManagerFactory} Spring needs to configure two beans {@link DataSource}
+ * and {@link JpaVendorAdapter}.
+ * <p>
+ * todo: 1. Mark this class as spring config
+ * todo: 2. Configure a bean of {@link DataSource}
+ * todo: 3. Configure a bean of {@link JpaVendorAdapter}
+ * todo: 4. Configure bean {@link javax.persistence.EntityManagerFactory} with name "entityManagerFactory"
+ *
+ */
+@Configuration
+public class JpaConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.H2);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true); // this sets hibernate.hbm2ddl.auto=update (Hibernate will generate db tables)
+        return adapter;
+    }
+
+    @Bean("entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean localContainerEMF(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setJpaVendorAdapter(jpaVendorAdapter);
+        emf.setPackagesToScan("com.bobocode.model");
+        return emf;
+    }
+}
